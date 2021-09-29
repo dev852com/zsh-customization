bc50fa2464270945ce17f369141506038a960cd0
sudocraft <88053027+sudocraft@users.noreply.github.com>
2021-09-30 05:21:10 +0800

    Update azure-zsh.sh

--- azure-zsh.sh
+++ azure-zsh.sh
@@ -2,7 +2,7 @@
 sudo apt update
 sudo apt install zsh -y
 sudo chsh -s $(which zsh) azure
-sudo touch ~/.zshrc
+touch ~/.zshrc
 sudo git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 sudo echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
