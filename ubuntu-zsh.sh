a35f3b4fe4ef69881eba864204f0f6eeb93298ca
Anthony Yung <88053027+sudocraft@users.noreply.github.com>
2021-10-07 23:41:37 +0800

    Update ubuntu-zsh.sh

--- ubuntu-zsh.sh
+++ ubuntu-zsh.sh
@@ -7,7 +7,9 @@ sudo git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autos
 sudo echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc
 sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.zsh/powerlevel10k
 sudo echo "source ~/.zsh/powerlevel10k/powerlevel10k.zsh-theme" >> ~/.zshrc
-sudo echo "POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true" >>! ~/.zshrc
+sudo echo "POWERLEVEL9K_DISABLE_CONFIGURATION_WIZARD=true" >> ~/.zshrc
 sudo git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh/zsh-syntax-highlighting
 sudo echo "source ~/.zsh/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ~/.zshrc
 sudo echo "alias ls='ls --color'" >> ~/.zshrc
+sudo apt install neofetch -y
+sudo echo "neofetch" >> ~/.zshrc
